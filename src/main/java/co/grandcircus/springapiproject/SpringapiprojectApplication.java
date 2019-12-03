cdc735331bb0cba43a1fcbd9fb8fc74fa8ea0033
flory-twin <flory.twin.2@gmail.com>
2019-12-03 13:31:55 -0500

    Initial commit.

---
+++ src/main/java/co/grandcircus/springapiproject/SpringapiprojectApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.springapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringapiprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/grandcircus/springapiproject/SpringapiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.springapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapiprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
