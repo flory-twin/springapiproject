de5a17258cd908e6085354722787c24723e23100
flory-twin <flory.twin.2@gmail.com>
2019-12-04 12:12:28 -0500

    Adds stub code for models and service.

---
+++ src/main/java/co/grandcircus/springapiproject/services/TheMovieDbService.java
@@ -0,0 +1,19 @@
+package co.grandcircus.springapiproject.services;
+
+public class TheMovieDbService {
+
+	public static final String baseUrl = "https://api.themoviedb.org/3/";
+	public static final String discoverResource = "/discover/movie";
+	
+	public TMDBServiceResponse searchMoviesByName(String name) {
+		return null;
+	}
+	
+	public TMDBServiceResponsesearchMoviesByYear(Integer year) {
+		return null;
+	}
+	
+	public TMDBServiceResponsesearchMoviesByYear(String genre) {
+		return null;
+	}
+}
